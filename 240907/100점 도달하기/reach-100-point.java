fa181392094b35f028a7fb43cefcefcb691e75cd
naong95 <sonic1@naver.com>
2024-09-07 20:08:14 +0900

    Title: 100점 도달하기, Time: 138ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240907/100점 도달하기/reach-100-point.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        StringBuilder sb = new StringBuilder("");
+
+        int n = sc.nextInt();
+
+        for(int i = n; i < 101; i++) {
+            if(i > 89) {
+                sb.append("A").append(" ");
+            } else if(i > 79) {
+                sb.append("B").append(" ");
+            } else if(i > 69) {
+                sb.append("D").append(" ");
+            } else if(i > 59) {
+                sb.append("D").append(" ");
+            } else {
+                sb.append("F").append(" ");
+            }
+        }
+
+        System.out.println(sb.toString());
+    }
+}
